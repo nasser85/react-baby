8f66503ae5b7b21d63535711298604c5342744b7
nasser85 <nasser.abouelazm@gmail.com>
2018-05-30 17:14:47 -0400

    add basic code for directory creation

---
+++ index.js
@@ -0,0 +1,36 @@
+var bluebird = require('bluebird');
+var path = require('path');
+var chalk = require('chalk');
+var ncp = bluebird.promisify(require('ncp').ncp);
+var rename = bluebird.promisify(require('fs').rename);
+
+ncp.limit = 16;
+
+var newProjectDir = (function () {
+
+    if (process.argv[2]) {
+        return path.resolve(process.cwd(), process.argv[2]);
+    }
+
+    return path.join(process.cwd(), 'react-baby-boilerplate');
+
+})();
+
+var generatorFilesPath = path.join(__dirname, 'react-baby-files');
+
+
+var copyFiles = function () {
+    return ncp(generatorFilesPath, newProjectDir);
+};
+
+var renameGitignore = function () {
+    var oldPath = path.join(newProjectDir, 'gitignore.txt');
+    var newPath = path.join(newProjectDir, '.gitignore');
+    return rename(oldPath, newPath);
+};
+
+console.log(chalk.green('Building your boilerplate...'));
+copyFiles().then(renameGitignore).then(function () {
+    console.log(chalk.green('Boilerplate generated successfully!'));
+    console.log(chalk.yellow('Project directory @ ' + newProjectDir));
+});
