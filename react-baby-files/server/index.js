731ec9d1c5fea15fba25a8f15c7073c89f506ffc
nasser85 <nasser.abouelazm@gmail.com>
2018-06-01 10:19:56 -0400

    add starter files

--- react-baby-files/server/index.js
+++ react-baby-files/server/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const chalk = require('chalk');
 
-const graphqlHTTP = require('express-graphql');
-//import schema from './schema';
 const publicPath = path.join(__dirname + '/../public');
 app.use('/', express.static(publicPath));
 
@@ -12,44 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(publicPath);
 });
 
-
-// const rawData = JSON.parse(fs.readFileSync('./books-test-collection.json', 'utf8'));
-// const inStock = rawData.filter(el=>el.active);
-// const prodLength = rawData.length;
-// const root = {
-//     getBooks: (data) => {
-//         let start = data.page*10 - 10;
-//         let query = data.active ? inStock : rawData;
-//         var res = [];
-//         for (var i = start; i < start + 10; i++) {
-//             res.push(query[i]);
-//         }
-//         return res;
-//     },
-//     searchBooks: (data) => {
-//         let query = data.search.toLowerCase();
-
-//         var res = [];
-//         for (var i = 0; i < prodLength; i++) {
-//             if (rawData[i].title.toLowerCase().indexOf(query) != -1) {
-//                 res.push(rawData[i]);
-//             }
-//             if (res.length >= 10) {
-//                 break;
-//             }
-//         }
-//         return res;
-//     }
-// }
-
-
-
-// app.use('/api', graphqlHTTP({
-//     schema: schema,
-//     rootValue: root,
-//     graphiql: true
-// }))
-
-app.listen(8000, () => {
-    console.log('server up n running on port 8000!');
+app.listen(8080, () => {
+    console.log(chalk.red('Server is running on 8080!'));
 })
